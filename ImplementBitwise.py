cef3cba0961b890400e0ecf39a11b56ebe5b3f80
aaravhajare <hajareaarav66@gmail.com>
2025-08-10 09:35:28 +0530

    update

---
+++ ImplementBitwise.py
@@ -0,0 +1,26 @@
+n1 =4
+n2 = 10
+
+
+# And
+print(n1 & n2)
+
+#OR
+print(n1 | n2)
+
+#NOt
+print(~n1)
+
+#XOR
+print(n1 ^ n2)
+
+#right shift 
+print(n1 >> 1)
+
+print(n2 >> 1)
+
+# left shift
+
+print(n1 << 1)
+
+print(n2 << 1)
